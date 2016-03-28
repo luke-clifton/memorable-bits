66b71379841b3f55396fc30a36a92b0d767fdcd3
Luke Clifton <ltclifton@gmail.com>
2016-03-29 08:24:40 +1100

    Added Food theme

---
+++ src/Data/Memorable/Theme/Food.hs
@@ -0,0 +1,54 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+module Data.Memorable.Theme.Food where
+
+import Data.Memorable
+
+type DessertFlavours = ToTree
+	'[ "strawberry"
+	 , "chocolate"
+	 , "vanilla"
+	 , "blueberry"
+	 , "raspberry"
+	 , "apple"
+	 , "almond"
+	 , "cherry"
+	 ]
+
+type DessertTypes = ToTree
+	'[ "custard"
+	 , "parfait"
+	 , "souffle"
+	 , "pancakes"
+	 , "icecream"
+	 , "tart"
+	 , "pie"
+	 , "cupcakes"
+	 ]
+
+type FoodAdjectives = ToTree
+	'[ "delicious"
+	 , "rancid"
+	 , "heavenly"
+	 , "scrumptious"
+	 , "delightful"
+	 , "disgusting"
+	 , "foul"
+	 , "exotic"
+	 , "bland"
+	 , "gourmet"
+	 , "tasty"
+	 , "tastless"
+	 , "refreshing"
+	 , "sensational"
+	 , "crunchy"
+	 , "creamy"
+	 ]
+
+type Desserts
+    = FoodAdjectives :<-> DessertFlavours :<-> DessertTypes
+
+desserts :: Proxy Desserts
+desserts = Proxy
