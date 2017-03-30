0bbcc0fe02be3db23ae8cf353a1b3baacf753035
Luke Clifton <ltclifton@gmail.com>
2017-03-30 13:33:47 +0800

    Version bounds and putUnaligned fix

--- default.nix
+++ default.nix
@@ -3,25 +3,10 @@ let
 in
 { nixpkgs ? import default-nixpkgs {}, compiler ? "default" }:
 let
-  # Until new version of bits is released on Hackage we need to use this.
-  bits-src = nixpkgs.pkgs.fetchFromGitHub
-    {
-      owner = "luke-clifton";
-      repo = "bits";
-      rev = "be7eea3931ebc951fd140c4829153067b0c8e0ac";
-      sha256 = "1v7hcswh4bqplj8pfs63n0hl069w5w2k9wdlz3j2y5cpv7fpgak8";
-    };
-
   haskellPackages =
     ( if compiler == "default"
       then nixpkgs.pkgs.haskellPackages
       else nixpkgs.pkgs.haskell.packages.${compiler}
-    ).override
-      {
-        overrides = self: super:
-        {
-          bits = self.callCabal2nix "bits" bits-src {};
-        };
-      };
+    );
 in
   haskellPackages.callPackage ./memorable-bits.nix { }
